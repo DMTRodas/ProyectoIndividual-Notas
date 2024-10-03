885dbfcef2a6add98fc01f628a2f3b74920512d3
Dulce♥ <dmonterrosor3@miumg.edu.gt>
2024-10-02 18:29:30 -0600

    Update escuela_db.sql

--- escuela_db.sql
+++ escuela_db.sql
@@ -1,37 +1,19 @@
--- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: escuela_db
--- ------------------------------------------------------
--- Server version	8.0.39
+CREATE DATABASE IF NOT EXISTS escuela_db;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+USE escuela_db;
 
---
--- Dumping data for table `notas_estudiantes`
---
+CREATE TABLE notas_estudiantes(
 
-LOCK TABLES `notas_estudiantes` WRITE;
-/*!40000 ALTER TABLE `notas_estudiantes` DISABLE KEYS */;
-INSERT INTO `notas_estudiantes` VALUES (12,'Stefano','Rossi',30,15,14,35,94),(14,'Ingrid','Villagran',34,13,12,32,91),(15,'Dulce','Monterroso',35,15,15,32,97);
-/*!40000 ALTER TABLE `notas_estudiantes` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+id_estudiante BIGINT AUTO_INCREMENT PRIMARY KEY,
+nombre_estudiante VARCHAR(255) NOT NULL,
+apellido_estudiante VARCHAR (255) NOT NULL,
+actividades INT,
+primer_parcial INT,
+segundo_parcial INT,
+examen_final INT,
+nota_final INT
+);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+SELECT * FROM notas_estudiantes;
 
 -- Dump completed on 2024-10-02 18:00:20
